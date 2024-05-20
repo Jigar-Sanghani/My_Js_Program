7d2f1cd90d5bdbfb16150379af4b367b24679c22
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-05-20 18:49:46 +0530

    js

--- Exam-2/script.js
+++ Exam-2/script.js
@@ -3,23 +3,59 @@ let products = []
 const handleproducts = (e) => {
     e.preventDefault();
     let data = {
-        image : document.getElementById("url").value,
-        name : document.getElementById("name").value,
-        price : document.getElementById("price").value
+        image: document.getElementById("url").value,
+        name: document.getElementById("name").value,
+        price: document.getElementById("price").value
     }
-    console.log(data.image,data.name,data.price);
-    // products.push(data);
+    console.log(data.image, data.name, data.price);
+    products.push(data);
+    uimaker();
+};
 
-    let image_value = document.createElement("img")
-    image_value.src = data.image;
-    let name_value = document.createElement("h1")
-    name_value.innerHTML = data.name;
-    let price_value = document.createElement("h2")
-    price_value.innerHTML = data.price;
-    let column = document.createElement('div')
-    column.append(image_value,name_value,price_value)
+const uimaker = () => {
+    document.getElementById("labber").innerHTML = "";
+    products.map((labber, index) => {
+        let image_value = document.createElement("img")
+        image_value.src = labber.image;
+        let name_value = document.createElement("h1")
+        name_value.innerHTML = labber.name;
+        let price_value = document.createElement("h2")
+        price_value.innerHTML = labber.price;
+        let buy_value = document.createElement("button")
+        buy_value.innerHTML = `BUY NOW`;
+        let delet_value = document.createElement("button")
+        delet_value.innerHTML = `REMOVE ITEM`;
 
-    document.getElementById("labber").append(column)
-}
+        let imgdiv = document.createElement("div");
+        imgdiv.setAttribute("class", "image_value");
+        imgdiv.append(image_value);
 
-document.getElementById("form").addEventListener("submit",handleproducts)
+        let btndiv = document.createElement("div");
+        btndiv.append(buy_value, delet_value);
+        btndiv.setAttribute("class", "btnDiv");
+
+        let div = document.createElement("div");
+        div.append(imgdiv, name_value, price_value, btndiv);
+        div.setAttribute("class", "div col-lg-3  col-md-4 col-sm-12");
+
+        document.getElementById("labber").append(div);
+        buy_value.addEventListener("click", buy_warning);
+        delet_value.addEventListener("click", () => Delete(index));
+    });
+};
+
+const buy_warning = () => {
+    alert("Product is Addedd to your Cart !! ");
+};
+
+const Delete_warning = () => {
+    alert("Product is Deleted !! ");
+};
+
+const Delete = (index) => {
+    Data.splice(index, 1);
+    Delete_warning();
+    uimaker();
+};
+
+document.getElementById("form").addEventListener("submit", handleproducts)
