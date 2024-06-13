f4d4c23479e0fe89ad18020334b1446bf4d2dc2c
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-06-13 17:11:23 +0530

    js

--- Project/js/index.js
+++ Project/js/index.js
@@ -1,25 +1,25 @@
 
 
-import navbar from "../components/navbar.js";
-import footer from "../components/footer.js";
-import slider from "../components/slider.js";
+// import navbar from "../components/navbar.js";
+// import footer from "../components/footer.js";
+// import slider from "../components/slider.js";
 
-document.getElementById("navbar").innerHTML = navbar()  
-document.getElementById("footer").innerHTML = footer()
-document.getElementById("slider").innerHTML = slider()
-
-
-
-let userdetails = JSON.parse(localStorage.getItem("user"));
-let isLogin = localStorage.getItem("isLogin") || false;
-if (!isLogin) {
-    window.location.href = "/Project/html/signup.html"
-}
+// document.getElementById("navbar").innerHTML = navbar()  
+// document.getElementById("footer").innerHTML = footer()
+// document.getElementById("slider").innerHTML = slider()
 
 
-if (userdetails) {
-    document.getElementById("navbar").innerHTML = navbar("Logout", userdetails.username)
-}
-else {
-    document.getElementById("navbar").innerHTML = navbar()
-}
+
+// let userdetails = JSON.parse(localStorage.getItem("user"));
+// let isLogin = localStorage.getItem("isLogin") || false;
+// if (!isLogin) {
+//     window.location.href = "/Project/html/signup.html"
+// }
+
+
+// if (userdetails) {
+//     document.getElementById("navbar").innerHTML = navbar("Logout", userdetails.username)
+// }
+// else {
+//     document.getElementById("navbar").innerHTML = navbar()
+// }
