021854b51fa50299025a581dd86c3936176a1a36
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-06-25 16:44:59 +0530

    js

--- Project/js/signup.js
+++ Project/js/signup.js
@@ -8,7 +8,7 @@ let isLogin = localStorage.getItem("isLogin") || false;
 let cartList = JSON.parse(localStorage.getItem("cartList")) || []
 let userdetails = JSON.parse(localStorage.getItem("user")) || []
 if (!isLogin) {
-    window.location.href = "/Project/html/signup.html"
+    // window.location.href = "/Project/html/signup.html"
 }
 if (userdetails) {
     document.getElementById("navbar").innerHTML = navbar("logout", userdetails.username)
