d284f5241a2fea67d8ee0dc851bd174ec9a69dcb
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-06-27 18:17:53 +0530

    js

---
+++ API-1/index.js
@@ -0,0 +1,26 @@
+
+const Mapper_Data = (data) => {
+    data.map((ele) => {
+        let img = document.createElement('img');
+        img.src = ele.image;
+        img.classList.add('img-fluid');
+
+        let title = document.createElement('h4');
+        title.innerHTML = ele.title;
+
+        let cost = document.createElement("h3")
+        cost.innerHTML = ele.price;
+
+        let div = document.createElement("div")
+        div.append(img, title, cost)
+
+        document.getElementById("Product_List").append(div)
+    })
+}
+
+const Data_Handle = async () => {
+    let request = await fetch("https://fakestoreapi.com/products")
+    let response = await request.json()
+    Mapper_Data(response)
+}
+Data_Handle()
