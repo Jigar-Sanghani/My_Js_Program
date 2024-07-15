fbad75f3ffb14cadb5f618128c49aafc2a92a55d
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-07-15 11:53:07 +0530

    js

---
+++ Audio_Player/index.js
@@ -0,0 +1,96 @@
+let music_system = [
+    {
+        audio: "https://rr1---sn-bu2a-nu8e.googlevideo.com/videoplayback?expire=1721031326&ei=PoaUZvNhpeLp2w-ijJCQCQ&ip=23.88.69.123&id=o-AHS-xfTa4hYQ_H4H3jrP30egv2_Zrlqh9NO4FcW-jfV0&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=5785128&dur=331.281&lmt=1720790430367382&keepalive=yes&c=ANDROID_TESTSUITE&txp=6308224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRAIgAiuaRHEwyb1xQYYls9Jw5ZPegsUXeUg936PKgnRzQSwCIClknapeweLRvo8EknypDUjqfOoGYE3XidiW4uQb52cv&redirect_counter=1&rm=sn-4g5eys7l&fexp=24350516,24350518&req_id=ef85097687dca3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=v5&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8e&ms=au&mt=1721020229&mv=u&mvi=1&pcm2cms=yes&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pcm2cms,pl&lsig=AHlkHjAwRQIhANq-DlaWI_RPpTs6M_F63kKfwKLG_1sqcnp1dNY9jtBkAiA9Mh4QOtBPFm0Oi7N0tz6tV0Qfvzr_tXZ5dX8d8TbXBQ%3D%3D",
+        name: " Angaaron",
+        image: "https://imgeng.jagran.com/images/2024/05/29/article/image/pushpa%202%20song%20anga-1716966063701.jpg",
+        icon: `<i class="fa-solid fa-music"></i>`
+    },
+    {
+        audio: "https://rr1---sn-bu2a-nu8s.googlevideo.com/videoplayback?expire=1721031743&ei=34eUZqXTKI7H6dsP3LO94Ag&ip=162.55.128.6&id=o-AKAMZfxGXfa75Y4wZuoQsT1bQnBbs2crCojZ2Exyn5Ib&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=2961114&dur=180.021&lmt=1714689297004367&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAN8NgHCfT4VWcqO0XfmPskteEEWf3miRuXPRpMUONBOmAiBjKAh5XVUgDKXyW-4oZOlWvB0OjSisbI47GiYYlKAZiw%3D%3D&redirect_counter=1&rm=sn-4g5edl7s&fexp=24350516,24350518&req_id=5e8c2e104cb3a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=iK&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8s&ms=au&mt=1721020229&mv=u&mvi=1&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIgdZ_ulA0boXWELnlpfU27UQ58G-v6B0iT9EuFpGb9EhwCIQCAXwXlshR_jjhb02AWR4ceXwRiba32wmjuXjaficZCbg%3D%3D",
+        name: " Arjun Vailly",
+        image: "https://akm-img-a-in.tosshub.com/sites/dailyo//resources/202311/blob211123053148.png",
+        icon: `<i class="fa-solid fa-music"></i>`
+    },
+    {
+        audio: "https://rr1---sn-bu2a-nu8l.googlevideo.com/videoplayback?expire=1721030118&ei=hoGUZt2mN72Ci9oPvcKf4AE&ip=188.40.52.24&id=o-AK348p2juOrHeeIxuVWBjIyxgy6zVfDr0xmJpTLFxy_A&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=4378433&dur=246.541&lmt=1718970148224647&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAMlXuEIiF9wis-l2Ql2sEMWtuedXq0crTmHx9hoVLB9tAiAcWIOeO9kqYZ0uNsiivBEID1jrB9c1XM1zYG-1cshgEA%3D%3D&redirect_counter=1&rm=sn-4g5ezl7e&fexp=24350516,24350518&req_id=54cce38a8c2aa3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=80&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8l&ms=au&mt=1721020229&mv=u&mvi=1&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRgIhAKqgU7zWU-3Ny-Ch_tOhgSYJIp5qsEg8o1E7Wg7Un2DoAiEA3K6cS8Odpirmkyy5d2rF73WkJSo0hbyyc73pz1M56oQ%3D",
+        name: " Bebe Bapu",
+        image: "https://i.ytimg.com/vi/cpKolP6mMec/maxresdefault.jpg",
+        icon: `<i class="fa-solid fa-music"></i>`
+    },
+    {
+        audio: "https://rr2---sn-bu2a-nu8e.googlevideo.com/videoplayback?expire=1721042124&ei=bLCUZspDtomL2g-CpI7YCg&ip=188.40.52.25&id=o-AA8SyiD-kEuAsGHw74rvz9ZXZgJo4XqxHohJsQd82Qo6&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3170064&dur=188.021&lmt=1714661994614462&keepalive=yes&c=ANDROID_TESTSUITE&txp=4502434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhALcCweUO2fbOP2RbE2lcHFjzFYyphuIM12U_m5anUaINAiBgIdRi63HnfqBzsyaFGpAzJrKd8Uhvh-U_EfdJRHvEsQ%3D%3D&redirect_counter=1&rm=sn-4g5ek67l&rrc=104&fexp=24350516,24350518&req_id=e7f750c3210aa3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=01&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8e&ms=au&mt=1721020229&mv=u&mvi=2&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIhAJglQrBuR1I_MzzhOdoDOIhS-0h5FQo-5hBRE7So0wBgAiBDJQAASF2H1lfGj2XPknPQxGrHUBQYQRo90MUiohvsmQ%3D%3D",
+        name: " Chaleya",
+        image: "https://i.ytimg.com/vi/wr9M-CoxP7A/maxresdefault.jpg",
+        icon: `<i class="fa-solid fa-music"></i>`
+    },
+    {
+        audio: "https://rr3---sn-bu2a-nu8s.googlevideo.com/videoplayback?expire=1721029359&ei=j36UZtWxMP7vi9oP6tWY4AI&ip=162.55.96.91&id=o-APUeiFhz269JSFHTp9sZa-rnThTk2H-4Y9Yaf4hktuN8&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=6840186&dur=380.541&lmt=1714644855114122&keepalive=yes&c=ANDROID_TESTSUITE&txp=4502434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRAIgK-hJHPvF2cIFHRSZH9VIsaNdiHB0htTcrR1Ni3SecokCIBTiithv7rV46Z4BuTz-Q-AEIa3zgish10bt8N4sFqZv&redirect_counter=1&rm=sn-4g5ez776&fexp=24350516,24350518&req_id=f58ac0edd3e4a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=P0&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8s&ms=au&mt=1721020229&mv=u&mvi=3&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIhAIvRWPGGNeiVByh1OUe0x456mRpLd_pvM2ITcuH-DtWxAiAbfxklR49ucXp2fje8XbgoKZfXLSUNXgeb69JJQdav5w%3D%3D",
+        name: " Kun Faaya Kun",
+        image: "https://i.ytimg.com/vi/T94PHkuydcw/maxresdefault.jpg",
+        icon: `<i class="fa-solid fa-music"></i>`
+    },
+    {
+        audio: "https://rr2---sn-bu2a-nu8e.googlevideo.com/videoplayback?expire=1721042861&ei=TbOUZp7rLtTQi9oP1Im0oAk&ip=23.88.69.123&id=o-AGgLOtpQIZ-2ziJWSaZBHJRnHcmJY0XhRpPZQN9tDUWW&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3472627&dur=193.101&lmt=1714896416813359&keepalive=yes&c=ANDROID_TESTSUITE&txp=4502434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIgC5jCOww0fyerWzGq1flLUrzGTPECTKopImBMjN6ohBACIQDnBq_WB2BCHxZCU_m65PDoB4eSCUh6vwWoKEgcJRJt-w%3D%3D&redirect_counter=1&rm=sn-4g5ezl7l&fexp=24350516,24350518&req_id=bb2bb8df6932a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=5w&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8e&ms=au&mt=1721020229&mv=u&mvi=2&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIgEMHGUoXluFbeaPu3zn2qd75Gs0Cd2n7LLtPWXx3-NisCIQDdaivNUSrsExkBxBm1UikD8lS9EjH2mYKowXnBmTGPHQ%3D%3D",
+        name: "Pushpa ",
+        image: "https://m.economictimes.com/thumb/msid-99219727,width-1200,height-900,resizemode-4,imgsize-59672/pushpa-2-shooting-stopped-and-2023-release-cancelled-heres-everything-we-know-about-allu-arjuns-upcoming-film.jpg",
+        icon: `<i class="fa-solid fa-music"></i>`
+    },
+    {
+        audio: "https://rr2---sn-bu2a-nu8s.googlevideo.com/videoplayback?expire=1721029286&ei=Rn6UZsz0N46li9oPhNmy-Ac&ip=188.40.52.27&id=o-AAJW1PzBLgAyAvmHdZIsQmhfHpGx7gFS-fm7GOhMIgqi&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=5933114&dur=345.301&lmt=1715745383134751&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAK-3O-WUYSJrdyWNHvVaaDfHijSkLQkTRfAjNbvgmU9LAiEAoemH2RhhNXQ6BagXeTXLi4DuXcC9OVSvVcCgpiLMzWQ%3D&redirect_counter=1&rm=sn-4g5err7e&fexp=24350516,24350518&req_id=8d448b971cb5a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=CY&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu8s&ms=au&mt=1721020229&mv=u&mvi=2&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRgIhAK0JHR7ynwXKM7hozEaBfvGbl4RLiotqSqaGwifgDxoQAiEAu_vM2ZTPmOQcuzbq-f8DeVxeGioUTN1PJjXBKWeVt9U%3D",
+        name: " Saari Duniya",
+        image: "https://i.ytimg.com/vi/6OXfgu8uKnE/maxresdefault.jpg",
+        icon: `<i class="fa-solid fa-music"></i>`
+    },
+    {
+        audio: "https://rr2---sn-bu2a-nu86.googlevideo.com/videoplayback?expire=1721041837&ei=Ta-UZrfIObSAi9oP1uORsAU&ip=188.40.52.27&id=o-AEgKRmWSZSmhF6IbUk5FfabEyWlmVbiaziktz8tjB9ZX&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=2675270&dur=146.021&lmt=1715977767066955&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAKgwvVnCXLmyXe8uVKsnYG0SxiVixfwouDzd0vYZhVQ5AiEA4--Vol_ZV9dLUhgE5UGdKlBvlm-4r37FJTBHZK-xlsM%3D&redirect_counter=1&rm=sn-4g5err7z&fexp=24350516,24350518&req_id=28d52c524699a3ee&cms_redirect=yes&cmsv=e&ipbypass=yes&mh=Sg&mip=27.116.48.74&mm=31&mn=sn-bu2a-nu86&ms=au&mt=1721020229&mv=u&mvi=2&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIgG1gSQBDd95uCBcoy0bntc1LsYyJ9Hy4IqXQA-12IqUQCIQChc2-wO0amldWqTDvbzaXZVwD44MKq5zVhLsYnEFPHZQ%3D%3D",
+        name: " Sajni",
+        image: "https://i.ytimg.com/vi/k3g_WjLCsXM/maxresdefault.jpg",
+        icon: `<i class="fa-solid fa-music"></i>`
+    },
+    {
+        audio: "https://rr3---sn-gwpa-w5py.googlevideo.com/videoplayback?expire=1721042979&ei=w7OUZqvcKLvmrtoP9NqWmAY&ip=2409%3A4085%3Adcc%3A6e24%3Abd77%3Ade0d%3Afd4d%3Afeb7&id=o-ABZ0s6ujRqsRpgRjkqr8i5e8Sje9fojDogS2qk9d7C_x&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=eo&mm=31%2C29&mn=sn-gwpa-w5py%2Csn-gwpa-qxae7&ms=au%2Crdu&mv=m&mvi=3&pl=41&gcr=in&initcwndbps=898750&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3328610&dur=209.541&lmt=1716262263941418&mt=1721020856&fvip=4&keepalive=yes&c=ANDROID_TESTSUITE&txp=4532434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIgca-PW9FHCw9v96e2yNxwDyanNu5qUmNJJIw_6q0afM0CIQD0edviTaf-gdlAPERy-K8wYMyY__k85Lpc37yqpQjiuw%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AHlkHjAwRgIhAKsQ3Hq8PRgdB3xnIvjfoXNeK_f4j5igQEbl5ZkAO_zfAiEAgVdNNOF8EXXOC4eCCLZmP3EZ7jhewtqtWaHvs8AYm-g%3D",
+        name: " Systumm",
+        image: "https://www.sarkariexam.com/wp-content/uploads/2023/08/Fastest-1-Million-Followers-On-Instagram-In-India-Elvish-Yadav-System-Hang.jpg   ",
+        icon: `<i class="fa-solid fa-music"></i>`
+    }
+]
+
+const mapper = (data) => {
+    data.map((ele) => {
+
+        let image = document.createElement("img");
+        image.src = ele.image;
+        image.classList.add("img-fluid", "image");
+
+        let audio = document.createElement("audio");
+        audio.src = ele.audio;
+        audio.controls = true;
+        audio.classList.add("audio");
+
+        let h1 = document.createElement("h1");
+        h1.innerHTML = ele.name;
+        h1.classList.add("h1");
+
+        let icon = document.createElement("icon");
+        icon.innerHTML = ele.icon;
+        icon.classList.add("icon");
+        icon.addEventListener("click", () => images(ele));
+
+        let div = document.createElement("div");
+        div.append(image, h1, audio,icon);
+        div.classList.add("div",);
+
+        document.getElementById("show_music").append(div);
+    })
+}
+
+const images = (ele) =>{
+    
+let img=  document.getElementById("images").src = (ele.image);
+    // img.classList.add("img-fluid","images")
+    let a = document.getElementById("auto")
+    a.src = ele.audio;
+
+}
+
+mapper(music_system);
