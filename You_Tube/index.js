68a3c5ec536565dc23ca3def862c7269c1607ccb
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-07-17 18:37:42 +0530

    js

--- You_Tube/index.js
+++ You_Tube/index.js
@@ -1,23 +1,27 @@
 const get_vedio = async (search) => {
     let req = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${search}&type=video&key=AIzaSyDNVBaE4O5vcC3LWmPtmk7Cnzx0GvmXWVA`)
-    let res = await req .json()
+    let res = await req.json()
     return res.items;
 }
 
-const   mapper = (data) => {
-    data.map((ele) => {   
+const mapper = (data) => {
+
+    document.getElementById("video_tag").innerHTML = ""
+
+    data.map((ele) => {
+
         let img = document.createElement('img')
         img.src = ele.snippet.thumbnails.medium.url
         img.classList.add('channel_image')
         img.addEventListener('click', () => {
             localStorage.setItem("Video_ID", JSON.stringify(ele.id.videoId))
-            window.location.href = "/You_Tube/Player.html"  
+            window.location.href = "/You_Tube/Player.html"
         })
 
         let title = document.createElement('h5')
         title.innerHTML = ele.snippet.title
         title.classList.add('title')
-        
+
         let channel_name = document.createElement('h4')
         channel_name.innerHTML = ele.snippet.channelTitle
         channel_name.classList.add('channel_name')
@@ -25,7 +29,7 @@ const   mapper = (data) => {
         let div = document.createElement('div')
         div.append(img, title, channel_name)
         div.classList.add('video_div')
-        
+
         document.getElementById('video_tag').append(div)
     });
 }
@@ -36,8 +40,6 @@ const Handle_Mapper = async (search) => {
 }
 
 const Search_Handle = (e) => {
-    e.preventDefault();
-    // e.preventDefault(); 
 
     if (e.key == "Enter") {
 
@@ -47,4 +49,4 @@ const Search_Handle = (e) => {
     }
 }
 
-document.getElementById("Video_Search").addEventListener("keypress",Handle)
+document.getElementById("Video_Search").addEventListener("keypress", Search_Handle)
