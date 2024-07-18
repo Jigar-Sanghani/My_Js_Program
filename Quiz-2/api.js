ebb9533b3f89db6f118c0b87fbc83532347f503f
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-07-18 17:52:54 +0530

    js

--- Quiz-2/api.js
+++
@@ -1,5 +0,0 @@
-const Qustion = async () => {
-    let req = await fetch (`https://json-server-fikb.onrender.com/qustion`);
-    let res = await req.json();
-    return res
-}
--- Quiz-2/script.js
+++ Quiz-2/script.js
@@ -1,139 +1,85 @@
-let question_array = [
-    {
-        "question": "How do you write 'Hello World' in an alert box?",
-        "option1": "msg('Hello World');",
-        "option2": "alertBox('Hello World');",
-        "option3": "alert('Hello World');",
-        "option4": "msgBox('Hello World');",
-        "answer": "alert('Hello World');"
-    },
-    {
-        "question": "Which of the following is not a reserved word in JavaScript?",
-        "option1": "interface",
-        "option2": "throws",
-        "option3": "program",
-        "option4": "short",
-        "answer": "program"
-    },
-    {
-        "question": "What is the correct way to write a JavaScript array?",
-        "option1": "var colors = (1:'red', 2:'green', 3:'blue')",
-        "option2": "var colors = 'red', 'green', 'blue'",
-        "option3": "var colors = ['red', 'green', 'blue']",
-        "option4": "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
-        "answer": "var colors = ['red', 'green', 'blue']"
-    },
-    {
-        "question": "Which event occurs when the user clicks on an HTML element?",
-        "option1": "onchange",
-        "option2": "onclick",
-        "option3": "onmouseclick",
-        "option4": "onmouseover",
-        "answer": "onclick"
-    },
-    {
-        "question": "How do you declare a JavaScript variable?",
-        "option1": "variable carName;",
-        "option2": "var carName;",
-        "option3": "v carName;",
-        "option4": "string carName;",
-        "answer": "var carName;"
-    },
-    {
-        "question": "What is the correct JavaScript syntax to change the content of the HTML element below?",
-        "code": "<p id='demo'>This is a demonstration.</p>",
-        "option1": "document.getElementById('demo').innerHTML = 'Hello World!';",
-        "option2": "document.getElementByName('demo').innerHTML = 'Hello World!';",
-        "option3": "document.getElementByTagName('p').innerHTML = 'Hello World!';",
-        "option4": "document.getElement('p').innerHTML = 'Hello World!';",
-        "answer": "document.getElementById('demo').innerHTML = 'Hello World!';"
-    },
-    {
-        "question": "How do you create a function in JavaScript?",
-        "option1": "function myFunction()",
-        "option2": "function:myFunction()",
-        "option3": "function = myFunction()",
-        "option4": "function => myFunction()",
-        "answer": "function myFunction()"
-    },
-    {
-        "question": "How do you call a function named 'myFunction'?",
-        "option1": "call myFunction()",
-        "option2": "call function myFunction()",
-        "option3": "myFunction()",
-        "option4": "Call.myFunction()",
-        "answer": "myFunction()"
-    },
-    {
-        "question": "What will the following code return: Boolean(10 > 9)?",
-        "option1": "false",
-        "option2": "true",
-        "option3": "NaN",
-        "option4": "undefined",
-        "answer": "true"
-    },
-    {
-        "question": "Which operator is used to assign a value to a variable?",
-        "option1": "*",
-        "option2": "x",
-        "option3": "=",
-        "option4": "-",
-        "answer": "="
-    },
-    {
-        "question": "Which method can be used to find the length of a string?",
-        "option1": "length()",
-        "option2": "getLength()",
-        "option3": "strlen()",
-        "option4": "length",
-        "answer": "length"
-    },
-    {
-        "question": "Which built-in method sorts the elements of an array?",
-        "option1": "changeOrder(order)",
-        "option2": "order()",
-        "option3": "sort()",
-        "option4": "arrange()",
-        "answer": "sort()"
-    },
-    {
-        "question": "Which of the following is a way to create an object in JavaScript?",
-        "option1": "var obj = {};",
-        "option2": "var obj = ();",
-        "option3": "var obj = [];",
-        "option4": "var obj = <>;",
-        "answer": "var obj = {};"
-    },
-    {
-        "question": "How do you round the number 7.25 to the nearest integer?",
-        "option1": "Math.rnd(7.25)",
-        "option2": "Math.round(7.25)",
-        "option3": "rnd(7.25)",
-        "option4": "round(7.25)",
-        "answer": "Math.round(7.25)"
-    },
-    {
-        "question": "How can you detect the client’s browser name?",
-        "option1": "navigator.appName",
-        "option2": "browser.name",
-        "option3": "client.navName",
-        "option4": "window.browser",
-        "answer": "navigator.appName"
-    },
-    {
-        "question": "Which JavaScript method is used to access an HTML element by id?",
-        "option1": "getElementById()",
-        "option2": "getElement(id)",
-        "option3": "getElementByID()",
-        "option4": "getIdElement()",
-        "answer": "getElementById()"
-    },
-]
 
-const UIMaker = () => {
-    question_array.map((ele, index) => {
+document.addEventListener('DOMContentLoaded', async function () {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+        const videoElement = document.createElement('video');
+        videoElement.autoplay = true;
+        videoElement.srcObject = stream;
+
+        document.getElementById('video').append(videoElement);
+    } catch (error) {
+        alert(" Your Camera Is Disable || ");
+    }
+});
+
+let tabSwitchTime = null;
+let awayTimeSeconds = 0;
+
+function calculateAwayTime() {
+    if (tabSwitchTime) {
+        let currentTime = new Date().getTime();
+        let timeDiff = currentTime - tabSwitchTime;
+        awayTimeSeconds += Math.round(timeDiff / 1000);
+        tabSwitchTime = null;
+    }
+}
+
+
+
+document.addEventListener('visibilitychange', function () {
+    if (document.visibilityState === 'hidden') {
+        tabSwitchTime = new Date().getTime();
+    } else if (document.visibilityState === 'visible' && tabSwitchTime !== null) {
+        calculateAwayTime();
+        alert(`You were away for ${awayTimeSeconds} seconds.`);
+        document.getElementById("away").innerHTML = `You were away for ${awayTimeSeconds} seconds.`;
+        awayTimeSeconds = 0;
+    }
+});
+
+function startAwayTimeCheck() {
+    setInterval(function () {
+        if (document.visibilityState === 'hidden' && tabSwitchTime !== null) {
+            calculateAwayTime();
+        }
+    }, 1000);
+}
+
+startAwayTimeCheck();
+
+const API = async () => {
+    let request = await fetch('https://json-server-2-e6hf.onrender.com/qustion');
+    let response = await request.json();
+    return response;
+}
+
+let Correct_Answers = 0
+
+const check = async (index, selectoption, btn) => {
+    let data = await API();
+    let correct = data[index].answer;
+
+    if (selectoption === correct) {
+        btn.style.backgroundColor = "green";
+        Correct_Answers++;
+    }
+    else {
+        alert("This Is Wrong Answer || ")
+        btn.style.backgroundColor = "red";
+    }
+
+    document.getElementById("Correct_Answer").innerHTML = ` Your Marks : ${Correct_Answers}`
+}
+
+const UIMaker = async () => {
+
+    let data = await API();
+
+    data.map((ele, index) => {
         let h1 = document.createElement("h1");
         h1.innerHTML = `${ele.question}`
+        h1.setAttribute("class", "title");
         let bt1 = document.createElement("button");
         bt1.innerHTML = `A. ${ele.option1}`
         let bt2 = document.createElement("button");
@@ -154,23 +100,10 @@ const UIMaker = () => {
     })
 }
 
-const check = (index, selectoption, btn) => {
-    const correct = question_array[index].answer;
-
-    if (selectoption === correct) {
-        btn.style.backgroundColor = "green";
-        // alert("Correct !!")
-    }
-    else {
-        btn.style.backgroundColor = "red";
-        // alert("Wrong !!")
-    }
-}
+UIMaker();
 
 const handleproducts = (e) => {
     e.preventDefault();
-    
-    question_array=[];
     let data = {
         question: document.getElementById("qs").value,
         option1: document.getElementById("op1").value,
@@ -179,31 +112,12 @@ const handleproducts = (e) => {
         option4: document.getElementById("op4").value,
         answer: document.getElementById("ans").value,
     }
-    question_array.push(data);
-    console.log(data);
     UIMaker();
 }
-
-UIMaker();
 
 document.getElementById("form").addEventListener("submit", handleproducts)
 
 document.addEventListener("contextmenu", (e) => {
     e.preventDefault();
-    alert("Right click is disabled!");
+    alert("Right Click Is Disabled ||");
 })
-
-let video = document.getElementById('video');
-if(navigator.mediaDevices.getUserMedia) {
-
-    navigator.mediaDevices.getUserMedia({video:true})
-    .then(function(s){
-        console.log(s);
-    });
-
-
-}
-
-else{
-    console.log("no");
-}
