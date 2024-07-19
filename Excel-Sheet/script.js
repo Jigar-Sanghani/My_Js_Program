22046b72eee1aef2a05e48d5edd0b487f2806fae
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-07-19 16:25:50 +0530

    js

--- Excel-Sheet/script.js
+++ Excel-Sheet/script.js
@@ -1,16 +1,16 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwxrr9gY3hHl86SuG0uv8nQa0NJHnmqFYqzCzrqM0w4wWDuE-B--j9r9iwch5FHZK6o/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwX5OUa2AJxz8uMu4RRKad83tH65akM_9wnpEk3hyyj3dsMeK92VkneHkha79El6klivg/exec'
 
 const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => alert("Thank you! your form is submitted successfully."))
+    .then(() => { window.location.reload(); })
+    .catch(error => console.error('Error!', error.message))
 })
 
 document.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-    alert("Right Click Is Disabled ||");
+  e.preventDefault();
+  alert("Right Click Is Disabled ||");
 })
