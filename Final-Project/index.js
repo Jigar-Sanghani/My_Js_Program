bf2d52965acc6bc8103b84007da7700b9ccc88bb
-Jigar-Sanghani <-jigarsanghani34@gmail.com>
2024-07-19 17:10:26 +0530

    js

---
+++ Final-Project/index.js
@@ -0,0 +1,18 @@
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            API({lat, lon}, false);
+        });
+    } else {
+        alert("Your Location Is Disabled || "); 
+    }
+}
+
+window.addEventListener("load", getLocation);
+
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    alert("Right Click Is Disabled ||");
+})
